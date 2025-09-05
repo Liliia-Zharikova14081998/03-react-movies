2ffaa91903bba36e1410f1b7f734cdc5e4e36df0
Liliia <liliyamalishnya@gmail.com>
2025-09-05 10:26:48 +0300

    update main.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,8 +4,12 @@ import type { Movie } from "../types/movie";
 const API_URL = "https://api.themoviedb.org/3/search/movie";
 const ACCESS_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
+interface MovieResponse {
+  results: Movie[];
+}
+
 export async function fetchMovies(query: string): Promise<Movie[]> {
-  const response = await axios.get(API_URL, {
+  const response = await axios.get<MovieResponse>(API_URL, {
     params: { query },
     headers: {
       Authorization: `Bearer ${ACCESS_TOKEN}`,
